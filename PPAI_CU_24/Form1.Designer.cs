ea4d7a6d1431b9c92b7225899ce230796103a468
efrain <efrainhost26@gmail.com>
2024-05-23 23:17:12 -0300

    pestaña_visual

--- PPAI_CU_24/Form1.Designer.cs
+++ PPAI_CU_24/Form1.Designer.cs
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            label7 = new Label();
+            groupBox1 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            button1 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
+            label1.ForeColor = Color.DarkRed;
+            label1.Location = new Point(250, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(299, 35);
+            label1.TabIndex = 0;
+            label1.Text = "Generar ranking de vino";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(24, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(314, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Seleccione fechas de consideracion de reseña:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(86, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Fecha hasta:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(81, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(94, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Fecha desde:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(181, 111);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(271, 27);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(181, 144);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(271, 27);
+            dateTimePicker2.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(24, 196);
+            label5.Name = "label5";
+            label5.Size = new Size(182, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Seleccione tipo de reseña:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(116, 231);
+            label6.Name = "label6";
+            label6.Size = new Size(59, 20);
+            label6.TabIndex = 7;
+            label6.Text = "Reseña:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(181, 228);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 8;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(24, 282);
+            label7.Name = "label7";
+            label7.Size = new Size(239, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Seleccione el tipo de visualizacion:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Location = new Point(116, 305);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(342, 67);
+            groupBox1.TabIndex = 10;
+            groupBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(6, 24);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(56, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "PDF";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(134, 26);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(64, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Excel";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(254, 26);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(82, 24);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Pantalla";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(358, 396);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 56);
+            button1.TabIndex = 11;
+            button1.Text = "Generar Ranking";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 464);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(label7);
+            Controls.Add(comboBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Generar informe ranking de vino";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox1;
+        private Label label7;
+        private GroupBox groupBox1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
     }
 }
