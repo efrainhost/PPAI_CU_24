0c1dfa791d49fdf9fff4269357aeb5ac5da02f24
efrain <efrainhost26@gmail.com>
2024-05-24 20:32:49 -0300

    pantalla terminada

--- PPAI_CU_24/PantallaGeneradorRanking.Designer.cs
+++ PPAI_CU_24/PantallaGeneradorRanking.Designer.cs
@@ -38,13 +38,8 @@
             label6 = new Label();
             cmbReseña = new ComboBox();
             label7 = new Label();
-            gbTipoDeVisualizacion = new GroupBox();
-            rdPantalla = new RadioButton();
-            rdExcel = new RadioButton();
-            rdPDF = new RadioButton();
             btnGenerar = new Button();
-            btnLimpiar = new Button();
-            gbTipoDeVisualizacion.SuspendLayout();
+            cmbTipoVisualizacion = new ComboBox();
             SuspendLayout();
             // 
             // label1
@@ -52,70 +47,68 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15F, FontStyle.Bold);
             label1.ForeColor = Color.DarkRed;
-            label1.Location = new Point(219, 15);
+            label1.Location = new Point(250, 20);
             label1.Name = "label1";
-            label1.Size = new Size(241, 28);
+            label1.Size = new Size(299, 35);
             label1.TabIndex = 0;
             label1.Text = "Generar ranking de vino";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(21, 57);
+            label2.Location = new Point(24, 76);
             label2.Name = "label2";
-            label2.Size = new Size(249, 15);
+            label2.Size = new Size(314, 20);
             label2.TabIndex = 1;
             label2.Text = "Seleccione fechas de consideracion de reseña:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(75, 112);
+            label3.Location = new Point(86, 149);
             label3.Name = "label3";
-            label3.Size = new Size(72, 15);
+            label3.Size = new Size(89, 20);
             label3.TabIndex = 2;
             label3.Text = "Fecha hasta:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(71, 88);
+            label4.Location = new Point(81, 117);
             label4.Name = "label4";
-            label4.Size = new Size(75, 15);
+            label4.Size = new Size(94, 20);
             label4.TabIndex = 3;
             label4.Text = "Fecha desde:";
             // 
             // dtFechaDesde
             // 
-            dtFechaDesde.Location = new Point(158, 83);
-            dtFechaDesde.Margin = new Padding(3, 2, 3, 2);
+            dtFechaDesde.Location = new Point(181, 111);
             dtFechaDesde.Name = "dtFechaDesde";
-            dtFechaDesde.Size = new Size(238, 23);
+            dtFechaDesde.Size = new Size(271, 27);
             dtFechaDesde.TabIndex = 4;
             // 
             // dtFechaHasta
             // 
-            dtFechaHasta.Location = new Point(158, 108);
-            dtFechaHasta.Margin = new Padding(3, 2, 3, 2);
+            dtFechaHasta.Location = new Point(181, 144);
             dtFechaHasta.Name = "dtFechaHasta";
-            dtFechaHasta.Size = new Size(238, 23);
+            dtFechaHasta.Size = new Size(271, 27);
             dtFechaHasta.TabIndex = 5;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(21, 147);
+            label5.Location = new Point(24, 196);
             label5.Name = "label5";
-            label5.Size = new Size(143, 15);
+            label5.Size = new Size(182, 20);
             label5.TabIndex = 6;
             label5.Text = "Seleccione tipo de reseña:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(102, 173);
+            label6.Location = new Point(117, 231);
             label6.Name = "label6";
-            label6.Size = new Size(47, 15);
+            label6.Size = new Size(59, 20);
             label6.TabIndex = 7;
             label6.Text = "Reseña:";
             // 
@@ -123,98 +116,47 @@
             // 
             cmbReseña.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbReseña.FormattingEnabled = true;
-            cmbReseña.Location = new Point(158, 171);
-            cmbReseña.Margin = new Padding(3, 2, 3, 2);
+            cmbReseña.Location = new Point(181, 228);
             cmbReseña.Name = "cmbReseña";
-            cmbReseña.Size = new Size(133, 23);
+            cmbReseña.Size = new Size(151, 28);
             cmbReseña.TabIndex = 8;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(21, 212);
+            label7.Location = new Point(24, 283);
             label7.Name = "label7";
-            label7.Size = new Size(188, 15);
+            label7.Size = new Size(239, 20);
             label7.TabIndex = 9;
             label7.Text = "Seleccione el tipo de visualizacion:";
             // 
-            // gbTipoDeVisualizacion
-            // 
-            gbTipoDeVisualizacion.Controls.Add(rdPantalla);
-            gbTipoDeVisualizacion.Controls.Add(rdExcel);
-            gbTipoDeVisualizacion.Controls.Add(rdPDF);
-            gbTipoDeVisualizacion.Location = new Point(102, 229);
-            gbTipoDeVisualizacion.Margin = new Padding(3, 2, 3, 2);
-            gbTipoDeVisualizacion.Name = "gbTipoDeVisualizacion";
-            gbTipoDeVisualizacion.Padding = new Padding(3, 2, 3, 2);
-            gbTipoDeVisualizacion.Size = new Size(299, 50);
-            gbTipoDeVisualizacion.TabIndex = 10;
-            gbTipoDeVisualizacion.TabStop = false;
-            // 
-            // rdPantalla
-            // 
-            rdPantalla.AutoSize = true;
-            rdPantalla.Location = new Point(222, 20);
-            rdPantalla.Margin = new Padding(3, 2, 3, 2);
-            rdPantalla.Name = "rdPantalla";
-            rdPantalla.Size = new Size(67, 19);
-            rdPantalla.TabIndex = 2;
-            rdPantalla.TabStop = true;
-            rdPantalla.Text = "Pantalla";
-            rdPantalla.UseVisualStyleBackColor = true;
-            // 
-            // rdExcel
-            // 
-            rdExcel.AutoSize = true;
-            rdExcel.Location = new Point(117, 20);
-            rdExcel.Margin = new Padding(3, 2, 3, 2);
-            rdExcel.Name = "rdExcel";
-            rdExcel.Size = new Size(52, 19);
-            rdExcel.TabIndex = 1;
-            rdExcel.TabStop = true;
-            rdExcel.Text = "Excel";
-            rdExcel.UseVisualStyleBackColor = true;
-            // 
-            // rdPDF
-            // 
-            rdPDF.AutoSize = true;
-            rdPDF.Location = new Point(16, 20);
-            rdPDF.Margin = new Padding(3, 2, 3, 2);
-            rdPDF.Name = "rdPDF";
-            rdPDF.Size = new Size(46, 19);
-            rdPDF.TabIndex = 0;
-            rdPDF.TabStop = true;
-            rdPDF.Text = "PDF";
-            rdPDF.UseVisualStyleBackColor = true;
-            // 
             // btnGenerar
             // 
-            btnGenerar.Location = new Point(278, 310);
+            btnGenerar.Location = new Point(343, 420);
+            btnGenerar.Margin = new Padding(3, 4, 3, 4);
             btnGenerar.Name = "btnGenerar";
-            btnGenerar.Size = new Size(75, 23);
+            btnGenerar.Size = new Size(86, 31);
             btnGenerar.TabIndex = 11;
             btnGenerar.Text = "generar";
             btnGenerar.UseVisualStyleBackColor = true;
             btnGenerar.Click += btnGenerar_Click;
             // 
-            // btnLimpiar
+            // cmbTipoVisualizacion
             // 
-            btnLimpiar.Location = new Point(371, 310);
-            btnLimpiar.Name = "btnLimpiar";
-            btnLimpiar.Size = new Size(75, 23);
-            btnLimpiar.TabIndex = 12;
-            btnLimpiar.Text = "limpiar";
-            btnLimpiar.UseVisualStyleBackColor = true;
-            btnLimpiar.Click += btnLimpiar_Click;
+            cmbTipoVisualizacion.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbTipoVisualizacion.FormattingEnabled = true;
+            cmbTipoVisualizacion.Location = new Point(181, 315);
+            cmbTipoVisualizacion.Name = "cmbTipoVisualizacion";
+            cmbTipoVisualizacion.Size = new Size(151, 28);
+            cmbTipoVisualizacion.TabIndex = 12;
             // 
             // FormGenerarRanking
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(700, 348);
-            Controls.Add(btnLimpiar);
+            ClientSize = new Size(800, 464);
+            Controls.Add(cmbTipoVisualizacion);
             Controls.Add(btnGenerar);
-            Controls.Add(gbTipoDeVisualizacion);
             Controls.Add(label7);
             Controls.Add(cmbReseña);
             Controls.Add(label6);
@@ -225,13 +167,10 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "FormGenerarRanking";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Generar ranking de vinos";
-            Load += Form1_Load;
-            gbTipoDeVisualizacion.ResumeLayout(false);
-            gbTipoDeVisualizacion.PerformLayout();
+            Load += PantallaGeneradorRanking_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -248,11 +187,7 @@
         private Label label6;
         private ComboBox cmbReseña;
         private Label label7;
-        private GroupBox gbTipoDeVisualizacion;
-        private RadioButton rdPantalla;
-        private RadioButton rdExcel;
-        private RadioButton rdPDF;
         private Button btnGenerar;
-        private Button btnLimpiar;
+        private ComboBox cmbTipoVisualizacion;
     }
 }
---
+++ PPAI_CU_24/PantallaGeneradorRanking.cs
@@ -0,0 +1,138 @@
+using System.Xml;
+
+namespace PPAI_CU_24
+{
+    public partial class FormGenerarRanking : Form
+    {
+        public FormGenerarRanking()
+        {
+            InitializeComponent();
+        }
+
+        private void PantallaGeneradorRanking_Load(object sender, EventArgs e)
+        {
+            cmbReseña.Items.Add("Normal");
+            cmbReseña.Items.Add("de Amigos");
+            cmbReseña.Items.Add("de Sommeliers");
+
+            cmbTipoVisualizacion.Items.Add("PDF");
+            cmbTipoVisualizacion.Items.Add("Excel");
+            cmbTipoVisualizacion.Items.Add("Pantalla");
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiarCampos();
+        }
+
+        private void limpiarCampos()
+        {
+            dtFechaDesde.Text = "";
+            dtFechaHasta.Text = "";
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            bool fechaValida = false;
+            bool reseñaSeleccionada = false;
+            bool tipoVisualizacionSeleccionada = false;
+
+            bool resultado = ValidarFecha(tomarFechaDesde(),tomarFechaHasta());
+            if (resultado)
+            {
+                fechaValida = true;
+            }
+            else
+            {
+                MessageBox.Show("Fechas invalidas");
+            }
+
+            if (tomarTipoReseña() == "")
+            {
+                MessageBox.Show("Debe seleccionar un tipo de reseña");
+            }
+            else
+            {
+                reseñaSeleccionada = true;
+            }
+
+            if (tomarTipoVisualizacion() == "")
+            {
+                MessageBox.Show("Debe seleccionar un tipo de visualizacion");
+            }
+            else
+            {
+                tipoVisualizacionSeleccionada = true;
+            }
+
+            if (fechaValida && reseñaSeleccionada && tipoVisualizacionSeleccionada)
+            {
+                solicitarConfirmacionReporte();
+            }
+           
+        }
+        private void solicitarConfirmacionReporte()
+        {
+            if (MessageBox.Show("¿Desea confirmar generacion de reporte?", "Confirmar registro", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                informarGeneracionExitosa();
+            }
+            else
+            {
+                MessageBox.Show("Se cancela la operacion");
+            }
+        }
+
+        private void informarGeneracionExitosa()
+        {
+            MessageBox.Show("Generacion exitosa");
+            this.Hide();
+        }
+
+        private DateTime tomarFechaDesde()
+        {
+            return dtFechaDesde.Value;
+        }
+        private DateTime tomarFechaHasta()
+        {
+            return dtFechaHasta.Value;
+        }
+
+        private string tomarTipoReseña()
+        {
+            return cmbReseña.Text;
+        }
+
+        private string tomarTipoVisualizacion()
+        {
+            return cmbTipoVisualizacion.Text;
+        }
+
+        private bool ValidarFecha(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            int resultado = fechaHasta.CompareTo(fechaDesde);
+            if (resultado == -1)
+            {
+                bool res = false;
+                return res;
+            }
+            else if (resultado == 1)
+            {
+                bool res = true;
+                return res;
+            }
+            else
+            {
+                bool res = false;
+                return res;
+            }
+        }
+
+    }
+
+}
+
+
+
+
+
