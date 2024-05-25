118ee9df78d6556a87c4dd54ac1779c4428feb8e
Mateo <mateoghiano10@gmail.com>
2024-05-25 16:03:29 -0300

    creacion de entidades

---
+++ PPAI_CU_24/Entidades/Provincia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI_CU_24.Entidades
+{
+    internal class Provincia
+    {
+    }
+}
