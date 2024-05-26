de4ebdebfa828561f5ac752f6a410dd1d9b96dab
Mateo <mateoghiano10@gmail.com>
2024-05-26 01:18:25 -0300

    actualizaciones[

--- PPAI_CU_24/Gestor/GestorGeneradorRankings.cs
+++ PPAI_CU_24/Gestor/GestorGeneradorRankings.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PPAI_CU_24.Entidades;
 
 namespace PPAI_CU_24.Gestor
 {
-    internal class GestorGeneradorRamkings
+    public class GestorGeneradorRankings
     {
+        // Atributos
+
+        // Constructor 
+
+        // Relaciones 
+        public Vino vino;
+
+        // Metodos
+        
+
+
     }
 }
